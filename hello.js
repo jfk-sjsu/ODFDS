f8d88a2c694cabae3c0b803c1b86cf182e2f0b4f
Kennedy <jkennedy@amd.com>
2020-04-10 14:10:54 -0700

    Update hello.js to work finally

--- hello.js
+++ hello.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const db = require('./connect-db')
+const db = require('./node/connect-db')
+app.use(express.static('html'))
 
-app.get('/', (req, res) => res.send(html/index.html))
+app.get('/', (req, res) => res.send(index.html))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
