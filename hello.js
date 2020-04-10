5685736efb56a0fc9291cad593fa7b554f68bf06
jfk-sjsu <61850948+jfk-sjsu@users.noreply.github.com>
2020-04-10 14:02:38 -0700

    Update hello.js

--- hello.js
+++ hello.js
@@ -3,6 +3,6 @@ const app = express()
 const port = 3000
 const db = require('./connect-db')
 
-app.get('/', (req, res) => res.send(db.connect()))
+app.get('/', (req, res) => res.send(html/index.html))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
