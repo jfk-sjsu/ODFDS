6759634073f4d1d4a584c96d85e8abaef5ec0b1b
Kennedy <jkennedy@amd.com>
2020-04-15 19:05:10 -0700

    login function example
    shows how the login page connectes to the nodejs werver. We can use this an example for all the other function s in the api.

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const reg = require('./driver')
+const driver = require('./driver')
 
 
 app.use(express.static('/'))
 
-
+app.post('/driver/Login', (req,res) => res.send(driver.login("email","password")));
 app.get('/', (req, res) => res.send(index.html));
 app.get('/driver/SignUp', function(req, res){ 
     
--- node/driver.js
+++ node/driver.js
@@ -1,26 +1,26 @@
 // driver.js all the driver functions in one spot
 
-function login ( email,  password) { 
+exports.login = function ( email,  password) { 
 // logs the driver in for work. requires password to be validated 
 	return "login stubb"; 
 } 
 
-function setActive ( id,  driverLat,  driverLong) { 
+exports.setActive = function( id,  driverLat,  driverLong) { 
 	// set's driver as active and records position. 
 	return " setActive stub"; 
 }
 
-function sendLocation ( id,  driverLat,  driverLong) { 
+exports.sendLocation = function ( id,  driverLat,  driverLong) { 
 // sends the drivers location to the database. 
 	return "sendLocation stub";
 }
 
-function ackOrderRequest ( id,  accept) {
+exports.ackOrderRequest = function( id,  accept) {
   //purpose: When an order is sent to a driver, the drivers calls this function to accept of decline. 
   // if accept, hand back the order id
   return "accOrderRequest stubb"
 }
-function ackCollectedOrder ( id,  dLat,  dLong,  ack) {
+exports.ackCollectedOrder = function( id,  dLat,  dLong,  ack) {
 /*	
   purpose: When driver is at restaurant, the system passes the order to the driver. This call acknowledges that the order is received.  
   params: 
@@ -34,7 +34,7 @@ function ackCollectedOrder ( id,  dLat,  dLong,  ack) {
 	return "ackCollectedOrder stub"; 
 }
 
-function ntfDeliveredOrder ( id,  dLat,  dLong, orderId) { 
+exports.ntfDeliveredOrder = function( id,  dLat,  dLong, orderId) { 
 /*
   purpose: sets the driver as "active" letting the system know that the driver is available for deliveries. 
   params: 
@@ -49,7 +49,7 @@ function ntfDeliveredOrder ( id,  dLat,  dLong, orderId) {
 }
 
 
-function ntfInactive ( id,  dLat,  dLong) { 
+exports.ntfInactive = function( id,  dLat,  dLong) { 
 /*  purpose: sets the driver as "inactive" letting the system know that the driver is not available for deliveries. 
   params: 
     driverID  type:driverID 
@@ -60,7 +60,7 @@ function ntfInactive ( id,  dLat,  dLong) {
 */
 	return "ntfInactive stub";
 }
-function Logout( id) {
+exports.logout = function ( id){
 /*	
   purpose: sets the driver as "inactive" letting the system know that the driver is not available for deliveries. Also, removes the 
     driver's ID from the system. For future calls, Login will be required. 
