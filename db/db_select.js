acaad1c2b11c0741d28c051c6cec94535300ea46
akshay-sud <37631614+akshay-sud@users.noreply.github.com>
2020-04-20 18:07:57 -0700

    Add files via upload

---
+++ db/db_select.js
@@ -0,0 +1,59 @@
+function verifyDriver(dLogin, dPW, callback) {
+    var mysql = require('mysql');
+    var con = mysql.createConnection({
+        host: "db",
+        user: "root",
+        password: "example",
+        database: "odfdsdb"
+    });
+    
+    con.connect(function(err) {
+        if (err) throw err;
+    });
+        
+        var sql = "SELECT DriverLogin, DriverPW from driver WHERE driverLogin = ?;";
+            
+        con.query(sql, [ dLogin ], function (err, result, fields) {
+            if (err) throw err;
+            
+            if (result[0].DriverLogin === dLogin && result[0].DriverPW === dPW) {
+                return callback(true);
+            }
+            else {
+                return callback(false);
+            }
+            
+        });
+    con.end();
+}
+
+function verifyRest(rLogin, rPW, callback) {
+    var mysql = require('mysql');
+    var con = mysql.createConnection({
+        host: "db",
+        user: "root",
+        password: "example",
+        database: "odfdsdb"
+    });
+    
+    con.connect(function(err) {
+        if (err) throw err;
+    });
+        
+        var sql = "SELECT RestLogin, RestPW from restaurant WHERE RestLogin = :login";
+            
+        con.query(sql, { login: rLogin }, function (err, result, fields) {
+            if (err) throw err;
+            
+            if (result[0].RestLogin === rLogin && result[0].DriverPW === rPW) {
+                return callback(true);
+            }
+            else {
+                return callback(false);
+            }
+            
+        });
+        
+    con.end();
+}
+
