dffb6ef51af055d64c34aa41a05dd58b2652c7e4
natishakhadgi <54525049+natishakhadgi@users.noreply.github.com>
2020-04-29 15:40:12 -0700

    Added css tags

--- website/tableify.js
+++ website/tableify.js
@@ -1,24 +1,23 @@
-function tableify(text, target) { 
-	var ret = "<table><tr><th>";
-	var json = JSON.parse(text); 
-	var header = json[0]; 
-	var fields = []; 
+function tableify(text, target) {
+	var ret = "<table id='tablify'><tr id='tablify'><th id='tablify'>";
+	var json = JSON.parse(text);
+	var header = json[0];
+	var fields = [];
 	for(var x in header) {
 		fields.push(x);
 		ret += x + "</th><th>"
 	}
-	ret = ret.substring(0, ret.length - 4); 
+	ret = ret.substring(0, ret.length - 4);
 	ret += "</tr></table>"
-	
-	json.forEach(mkrow); 
-	function mkrow(item,index) { 
+
+	json.forEach(mkrow);
+	function mkrow(item,index) {
 		ret = ret.replace("</table>", "<tr>");
-		
+
 		for(x in item) {
 			ret += "<td>" + item[x] + "</td>";
 		}
 		ret += "</tr></table>";
-		target.innerHTML = ret; 
+		target.innerHTML = ret;
 	}
-} 
-
+}
