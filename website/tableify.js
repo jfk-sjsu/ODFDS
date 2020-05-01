6a9d44e15e086fac44ef6b9d60cc5f7878c7a5da
jfk-sjsu <61850948+jfk-sjsu@users.noreply.github.com>
2020-04-30 22:22:44 -0700

    Adds code to make the able clickable!

--- website/tableify.js
+++ website/tableify.js
@@ -1,4 +1,17 @@
 function tableify(text, target) {
+	var scrpt = '<script>
+  function reverse(elem) { 
+  //	 alert("alert");
+  	 elem.innerHTML = elem.innerHTML.split("").reverse().join("");//elem.innerHTML.reverse();
+  }
+  var ths = getElementByClassName('header');
+//  Array.from(elements).forEach(function(element) {
+ //     alert("blah");
+//      element.addEventListener('click', reverse(this));
+//    });
+  
+</script>'
+
 	var ret = "<table id='tablify'><tr id='tablify'><th id='tablify'>";
 	var json = JSON.parse(text);
 	var header = json[0];
